8af0bd7437f31cedec1b693b1c1ebd9bf7f4422b
harshva89 <harshvaswani89@gmail.com>
2021-04-03 17:44:37 +0530

    Layout of list and each item page

--- app/src/main/java/com/example/ki_shaan/EachShopItemPage.java
+++ app/src/main/java/com/example/ki_shaan/EachShopItemPage.java
@@ -3,15 +3,16 @@ package com.example.ki_shaan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class ShopActivity extends AppCompatActivity {
+public class EachShopItemPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shop);
+        setContentView(R.layout.activity_each_shop_item_page);
     }
 
-
-
+    public void buyThisItemClick(View view) {
+    }
 }
