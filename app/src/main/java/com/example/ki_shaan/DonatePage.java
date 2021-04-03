b3d5f3f22a7a84a944372e2d89c947b64615eebb
harshva89 <harshvaswani89@gmail.com>
2021-04-03 18:59:08 +0530

    donate page

--- app/src/main/java/com/example/ki_shaan/DonatePage.java
+++ app/src/main/java/com/example/ki_shaan/DonatePage.java
@@ -3,6 +3,7 @@ package com.example.ki_shaan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class DonatePage extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class DonatePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donate_page);
+    }
+
+    public void onClickDonate(View view) {
     }
 }
