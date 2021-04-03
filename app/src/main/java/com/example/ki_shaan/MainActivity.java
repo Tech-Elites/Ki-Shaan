96ba051cd899ff55ad9348d31b02cf3dbbd1f7f8
sonashubhamvats <sona.svats23@gmail.com>
2021-04-03 15:04:30 +0530

    adding the bg

--- app/src/main/java/com/example/ki_shaan/MainActivity.java
+++ app/src/main/java/com/example/ki_shaan/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         finish();
-        startActivity(new Intent(this, RegisterPage.class));
+        startActivity(new Intent(this, LoginPage.class));
     }
 }
