59c374d9295e302288be6ed6e08d40600c8c88bf
harshva89 <harshvaswani89@gmail.com>
2021-04-04 01:06:47 +0530

    Classes and selling page

--- app/src/main/java/com/example/ki_shaan/MainActivity.java
+++ app/src/main/java/com/example/ki_shaan/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         finish();
-        startActivity(new Intent(this, LoginPage.class));
+        startActivity(new Intent(this, FarmerSell.class));
     }
 
 }
