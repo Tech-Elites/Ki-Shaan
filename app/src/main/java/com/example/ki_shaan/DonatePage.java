553d6ae991fcae0e2c62b2816af4ed60249e3e3f
Devansh0512 <devanshmehra108@gmail.com>
2021-04-04 05:37:37 +0530

    contact_us and donation final

--- app/src/main/java/com/example/ki_shaan/DonatePage.java
+++ app/src/main/java/com/example/ki_shaan/DonatePage.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,30 +13,39 @@ import android.widget.EditText;
 public class DonatePage extends AppCompatActivity {
 
     Button btn1;
+    String amt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donate_page);
-        btn1=findViewById(R.id.button4);
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v)
-            {
-                AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(DonatePage.this);
-                dlgAlert.setMessage("Thank you for your valuable donation!!");
-                dlgAlert.setTitle("Thank you!");
-                dlgAlert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        EditText edittext = findViewById(R.id.DontationPageAmount);
-                        edittext.setText("");
-                    }
-                }).setCancelable(true);
-                dlgAlert.create().show();
-            }
-        });
     }
 
-    public void onClickDonate(View view) {
+    public void donatePagedonate(View view) {
+        EditText t1 = findViewById(R.id.DontationPageAmount);
+        amt = t1.getText().toString();
+        Donate(amt);
+    }
+    public void Donate(String amt) {
+        if(TextUtils.isEmpty(amt)){
+            new android.app.AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_info)
+                .setTitle("Details not filled!")
+                .setMessage("Fill all the details to proceed.")
+                .setPositiveButton("Ok", null)
+                .show();
+        }
+        else{
+            AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(DonatePage.this);
+            dlgAlert.setMessage("Thank you for your valuable donation!!");
+            dlgAlert.setTitle("Thank you!");
+            dlgAlert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    EditText edittext = findViewById(R.id.DontationPageAmount);
+                    edittext.setText("");
+                }
+            }).setCancelable(true);
+            dlgAlert.create().show();
+        }
     }
 }
